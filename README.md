fcad830178962ff80d7da1cef176d1736f915b06
Oleksandr Mosur <oleksandr.mosur@gmail.com>
2018-08-15 20:39:18 +0300

    Add src

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# cql-calculator
+# Calculating the size of a table in Cassandra
+
+Formula:
+![](https://github.com/johnnywidth/cql-calculator/raw/master/src/size-formula.png "Formula")
+
+> go run main.go -file example.yaml
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+const MEGABYTE = 1.0 << (10 * 2)
+
+type Metadata struct {
+	Name      string   `yaml:"name"`
+	Rows      int      `yaml:"rows"`
+	Partition []Column `yaml:"partition"`
+	Cluster   []Column `yaml:"cluster"`
+	Static    []Column `yaml:"static"`
+	Column    []Column `yaml:"column"`
+}
+
+type Column struct {
+	Name string `yaml:"name"`
+	Type string `yaml:"type"`
+	Size int    `yaml:"size"`
+}
+
+var m Metadata
+
+func main() {
+	fileName := flag.String("file", "", "")
+	flag.Parse()
+
+	f, err := ioutil.ReadFile(*fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	m = Metadata{}
+	err = yaml.Unmarshal(f, &m)
+	if err != nil {
+		panic(err)
+	}
+
+	nov := numberOfValues(m)
+	log.Printf("Number of Values:\n%d\n\n", nov)
+
+	pds := partitionDiskSize(m, nov)
+	log.Printf("Partition Size on Disk:\n%d bytes\n%.2f Mb", pds, float64(pds)/MEGABYTE)
+}
+
+func numberOfValues(t Metadata) int {
+	allColumns := len(t.Column) + len(t.Partition) + len(t.Cluster) + len(t.Static)
+	primaryColumns := len(t.Partition) + len(t.Cluster)
+	staticColumns := len(t.Static)
+
+	log.Printf("Number of Values(%d*(%d-%d-%d) + %d)", t.Rows, allColumns, primaryColumns, staticColumns, staticColumns)
+
+	return t.Rows*(allColumns-primaryColumns-staticColumns) + staticColumns
+}
+
+func partitionDiskSize(t Metadata, nov int) int {
+	var sofPK int
+	for _, v := range t.Partition {
+		sofPK += v.Size
+	}
+
+	var sofS int
+	for _, v := range t.Static {
+		sofS += v.Size
+	}
+
+	var sofCK int
+	for _, v := range t.Cluster {
+		sofCK += v.Size
+	}
+
+	var ek int
+	for _, v := range t.Column {
+		ek += v.Size + sofCK
+	}
+
+	log.Printf("Partition Size on Disk(%d + %d + (%d * %d) + (8 * %d))", sofPK, sofS, t.Rows, ek, nov)
+
+	return sofPK + sofS + (t.Rows * ek) + (8 * nov)
+}
